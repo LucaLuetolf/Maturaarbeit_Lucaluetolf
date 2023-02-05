4c24c92aa8db3e69aa5be3ef1ea246159593d6af
LucaLuetolf <luca.luetolf@bluewin.ch>
2023-02-05 15:16:13 +0100

    test123

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module lucaluetolf.maturaarbeit_lucaluetolf {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens lucaluetolf.maturaarbeit_lucaluetolf to javafx.fxml;
+    exports lucaluetolf.maturaarbeit_lucaluetolf;
+}
