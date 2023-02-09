a0c601266443d2ab21467771cb66e95751b30dcf
LucaLuetolf <luca.luetolf@bluewin.ch>
2023-02-09 16:19:56 +0100

    Erstellt am 09/02/2023

---
+++ src/main/java/lucaluetolf/maturaarbeit_lucaluetolf/TestRun.java
@@ -0,0 +1,11 @@
+package lucaluetolf.maturaarbeit_lucaluetolf;
+
+public class TestRun {
+
+    public static void main(String[] args) {
+        Kunden.setupKunden();
+        Kunden.tester();
+
+    }
+
+}
