546b7350fa5d2fc1e292343ea14c8ef145ca212c
LucaLuetolf <luca.luetolf@bluewin.ch>
2023-06-12 12:27:39 +0200

    Die Seite Artikel und Kunden wurden angefangen, jedoch sind sie nicht Fertig. Das Fenster neue Rechnung ist in bearbeitung.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,10 @@ module lucaluetolf.maturaarbeit_lucaluetolf {
     requires javafx.fxml;
     requires kernel;
     requires layout;
-
+    requires io;
+    requires java.sql;
+    requires com.jfoenix;
+    requires java.desktop;
 
     opens lucaluetolf.maturaarbeit_lucaluetolf to javafx.fxml;
     exports lucaluetolf.maturaarbeit_lucaluetolf;
